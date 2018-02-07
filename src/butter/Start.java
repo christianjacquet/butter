dc8697082b42c672a1cff90c1b74a7c1b62557a2
Christian Jacquet <christian.jacquet@gmail.com>
2018-02-07 14:37:48 +0100

    Butter is on its way...

---
+++ src/butter/Start.java
@@ -0,0 +1,10 @@
+package butter;
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Butter is on it´s way...");
+		}
+
+}
