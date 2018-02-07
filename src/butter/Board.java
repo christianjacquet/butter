646dab54fa3a1c65240ac04c78c3c646a2c10e3f
Christian Jacquet <christian.jacquet@gmail.com>
2018-02-07 20:34:14 +0100

    Added board initialization

--- src/butter/Board.java
+++ src/butter/Board.java
@@ -1,11 +1,13 @@
 package butter;
 
 import java.lang.reflect.Array;
+import java.util.Random;
 
 public class Board {
 	static int[][] board;
 	static int numberOfFieldsX;
 	static int numberOfFieldsY;
+	static Random random = new Random();
 	
 	public Board(int numberOfFieldsX,int numberOfFieldsY) {
 		this.numberOfFieldsX = numberOfFieldsX;
@@ -18,6 +20,25 @@ public class Board {
 			for (int y=0; y<numberOfFieldsY; y++) {
 				board[x][y]=0;
 			}
+		}
+	}
+	
+	public static void populate(int numberOfObjectTypes) {
+		for (int x=0; x<numberOfFieldsX; x++) {
+			for (int y=0; y<numberOfFieldsY; y++) {
+				board[x][y]=random.nextInt((numberOfObjectTypes));
+				System.out.print(board[x][y]+" ");
+			}
+			System.out.println("");
+		}
+	}
+	
+	public static void print() {
+		for (int x=0; x<numberOfFieldsX; x++) {
+			for (int y=0; y<numberOfFieldsY; y++) {
+				System.out.print(board[x][y]+" ");
+			}
+			System.out.println("");
 		}
 	}
 
--- src/butter/Start.java
+++ src/butter/Start.java
@@ -5,11 +5,21 @@ public class Start {
 	// data
 	private int numOfFieldsX = 10;
 	private int numOfFieldsY = 10;
+	Board gameBoard;
+	static Start game;
+	
+	public Start() {
+		gameBoard = new Board(numOfFieldsX,numOfFieldsY);
+		gameBoard.clear();
+		gameBoard.populate(10);
+		gameBoard.print();
+	}
 	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Butter is on it´s way...");
+		game = new Start();
 		}
 
 }
